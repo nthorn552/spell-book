ce95db797c7b653d78181da43f72c68241d6b33d
Nathaniel Thorn <nate@arcsource.com>
2019-07-22 16:46:15 -0700

    update to ipmi script

---
+++ scripts/ipmi-update-power-state.ps1
@@ -0,0 +1,31 @@
+﻿param (
+    [string]$User = $(throw "-User is required."), 
+    [string]$Password = $(throw "-Password is required."), 
+    [string]$IPAddress = $(throw "-IPAddress is required."),
+    [switch]$Stop = $false,
+    [switch]$Start = $false
+)
+
+Import-Module pcsvdevice
+
+$EncryptedPassword = ConvertTo-SecureString -String "$Password" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $EncryptedPassword
+Write-Host "Attempting fetch device at $IPAddress"
+Try {
+    $Device = Get-PcsvDevice -TimeoutSec 20 -TargetAddress $IPAddress -ManagementProtocol IPMI -Credential $Credential
+    if ($Start) {
+        Write-Host "Device found, attempting start"
+        $Device | Start-PcsvDevice -AsJob
+        Write-Host "Start successful"
+    }
+    elseif ($Stop) {
+        Write-Host "Device found, attempting stop"
+        $Device | Stop-PcsvDevice -AsJob
+        Write-Host "Stop successful"
+    }
+}
+Catch {
+    Write-Host "Unable to connect to device"
+    Break
+}
+Break
